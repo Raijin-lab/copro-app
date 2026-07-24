15bc3dff47fd449af938a604b6fbf0f6d0f5b63d
Raijin-lab <zouhair.games@gmail.com>
2026-07-24 17:39:37 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'syndic-cache-v35'; // On passe à la v10 pour forcer la purge
+const CACHE_NAME = 'syndic-cache-v36'; // On passe à la v10 pour forcer la purge
 const urlsToCache = [
     './',
     './index.html',
